76b1130e7f71ac0b252f52ea5ca0103e90f66880
Akash Kola <63763012+Akashnani-star@users.noreply.github.com>
2022-09-28 11:46:30 +0530

    Update BuildStep.groovy

--- src/com/akashnani/BuildStep.groovy
+++ src/com/akashnani/BuildStep.groovy
@@ -2,7 +2,8 @@ package com.akashnani
 class BuildStep{
   def build(sc){
     sc.echo "buildied"
-    hello = sc.sh returnStdout: true, script: "echo hello"
+    def hello = sc.sh returnStdout: true, script: "echo hello"
+    sc.println hello
     sc.println currentBuild
   }
   def buildVerify(sc){
